72a2808af77631f5c5651bb5c2da3c9041be27b1
Domur <DrewBlu123@outlook.com>
2017-06-10 19:16:25 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-function myFunc(){
-alert('LUL');
+var twoSelected = false;
+var count = 0;
+function myFunc(numSelected){
+var x = document.getElementsByClassName("box");
+var y = document.getElementsByClassName("box2");
+
+if(count < 2){
+x[numSelected - 1].style.display = "none";
+y[numSelected - 1].style.display = "inline-block";
+count++;
+}
+else{
+for(var i = 0; i < x.length; i++){
+ y[i].style.display = "none";
+ x[i].style.display = "inline-block";
+}
+x[numSelected - 1].style.display = "none";
+y[numSelected - 1].style.display = "inline-block";
+count = 1;
+}
 }
